4dc591bb585e70a1ab101ebd182b42a41628e369
Jonathan Lindell <jonathanlindell9@gmail.com>
2024-01-10 23:59:09 +0100

    New Test Website

---
+++ script.js
@@ -0,0 +1,10 @@
+function toggleMenu() {
+    if (document.getElementById("menu").style.display === "none") 
+    {
+        document.getElementById("menu").style.display = "flex";
+    } 
+    else 
+    {
+        document.getElementById("menu").style.display = "none";
+    }
+}
